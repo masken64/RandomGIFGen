74deac7276699a1a667ca7fea67fd393f9ee8db5
masken64 <shrkartik2001@gmail.com>
2024-04-07 13:35:48 +0530

    add error handling and async

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 const gif = document.querySelector("#gifcontainer");
-const keyword = prompt("enter a keyword");
-
-
-
-
 //api key public becuase its free tier and rate limited to 100 calls per hour so not an issue
-if (keyword) {
-  fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=zz4wzmlG06dqGdYdNm6GsiJEaOiL5gS0&s=${keyword}`,
-    {
-      mode: "cors",
-    },
-  )
-    .then(handleResponse)
-    .then(logResponse);
+async function getCats(){
+  try{
+  const keyword = await prompt("enter a keyword");
+  const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=zz4wzmlG06dqGdYdNm6GsiJEaOiL5gS0&s=${keyword}`,{mode: "cors",},);
+  const catData = await response.json();
+  gif.src = catData.data.images.original.url;
+}catch(err){
+  console.error(err);
+  gif.src = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzB2aWFodmxzcTRhaHZ6bHYxYmVncG85YWVoa2gxcnZqeGZpMXBuaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sE1fbQPozKg3q5I2W2/giphy.gif";
 }
-
-function handleResponse(response) {
-  return response.json();
 }
-function logResponse(response) {
-  const currImage = response.data.images.original.url;
-  gif.src = currImage;
-}
+getCats();
